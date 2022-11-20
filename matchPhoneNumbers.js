9b3ab0a72f75b15bcb53808872996dc0089cacba
VelizarDabov <112800258+VelizarDabov@users.noreply.github.com>
2022-11-20 09:18:44 +0200

    Add files via upload

---
+++ matchPhoneNumbers.js
@@ -0,0 +1,9 @@
+function phoneNumbers(phones){
+let pattern = /\+359([ -])2\1\d{3}\1\d{4}\b/g;
+let text = phones.shift();
+
+let result = text.match(pattern);
+console.log(result.join(', '));
+
+}
+phoneNumbers(['+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222'])
